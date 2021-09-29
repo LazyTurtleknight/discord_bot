af51965a3e83d6e133c635abd1cbf4619792adc8
Simon Drees <tube3719gmail.com>
2021-09-29 11:26:06 +0200

    Project setup with npm

---
+++ bot.js
@@ -0,0 +1,5 @@
+console.log('Hello World');
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.login('ODkyNDEzMjQ5MjY0NDMxMTM1.YVMimg.fZfPZMs2RMfZHup2favpMzewcSY');
