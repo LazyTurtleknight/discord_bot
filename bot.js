6436cf38efcf63de43d0d7072e3074bb5dc370f1
LazyTurtleknight <tube3719@gmail.com>
2021-09-29 12:48:20 +0200

    Update bot initialization due to discordjs version 13 which specifies that bots need to have intents indicating which events they need

--- bot.js
+++ bot.js
@@ -2,5 +2,18 @@ console.log('Hello World');
 require('dotenv').config();
 
 const Discord = require('discord.js');
-const client = new Discord.Client();
+/**
+ * Setup bot client with intents. Note that the intents spcify which events the bot
+ * will receive.
+ * TODO: add intents once the bot needs more event to work with!
+ */
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
 client.login(process.env.DISCORD_TOKEN);
+
+client.on('ready', readyDiscord);
+
+function readyDiscord() {
+  console.log('I am ready!')
+};
