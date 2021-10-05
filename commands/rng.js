89beca142e9d207eaaf0f866ecbf947fd014df1d
LazyTurtleknight <tube3719@gmail.com>
2021-10-05 13:37:13 +0200

    Adapt rng slash command to have the epemeral option meaning the reply can only be seen by the person using the command

--- commands/rng.js
+++ commands/rng.js
@@ -11,20 +11,27 @@ module.exports = {
       .addIntegerOption((option) => option
           .setName('upperbound')
           .setDescription('Biggest possible number.')
-          .setRequired(true)),
+          .setRequired(true))
+      .addBooleanOption((option) => option
+          .setName('hidden')
+          .setDescription('Shall the answer only be seen by you ?.')),
   async execute(interaction) {
     const lowerBound = interaction.options.getInteger('lowerbound');
     const upperBound = interaction.options.getInteger('upperbound');
+    const ephemeral = interaction.options.getBoolean('hidden');
     if (lowerBound >= upperBound) {
-      await interaction.reply(
-          `You are a funny one huh.\n` +
-          `Try again :)`);
+      await interaction.reply({
+        content: `You are a funny one huh.\n`+
+        `Try again :)\n`+
+        `The lower bound should be bigger than the upper bound.`,
+        ephemeral: ephemeral});
     } else {
       const min = Math.ceil(lowerBound);
       const max = Math.floor(upperBound);
       const answer = Math.floor(Math.random() * (max - min +1)) + min;
-      await interaction.reply(
-          `It is a ${answer}.`);
+      await interaction.reply({
+        content: `It is a ${answer}.`,
+        ephemeral: ephemeral});
     }
   },
 };
