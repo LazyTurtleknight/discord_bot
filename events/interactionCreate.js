7d4870c4d8791aaae1911df5792ca308af6327da
LazyTurtleknight <tube3719@gmail.com>
2021-10-08 13:17:39 +0200

    Adapt event handler to call appropriate handler for different event type: isButton, command and isSelectMenu

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,26 +1,58 @@
 module.exports = {
   name: 'interactionCreate',
   execute(interaction) {
-    console.log(`${interaction.user.tag} in #${interaction.channel.name}` +
+    console.log(`${interaction.user.tag} in #${interaction.channel.name} ` +
     `triggered an interaction.`);
 
     // command handler
-    // return if interaction is not a command
-    if (!interaction.isCommand()) return;
+    if (interaction.isCommand()) {
+      const command = interaction.client.commands.get(
+          interaction.commandName);
 
-    const command = interaction.client.commands.get(interaction.commandName);
+      // return if command does not exist
+      if (!command) return;
 
-    // return if command does not exist
-    if (!command) return;
+      // reply by executing appropriate command
+      try {
+        command.execute(interaction);
+      } catch (error) {
+        console.error(error);
+        interaction.reply({
+          content: 'There was an error while executing this command',
+          ephemeral: true});
+      }
+    } else if (interaction.isSelectMenu()) {
+      const SelectMenuEvent = interaction.client.selectMenu
+          .get(interaction.component.customId);
 
-    // reply by executing appropriate command
-    try {
-      command.execute(interaction);
-    } catch (error) {
-      console.error(error);
-      interaction.reply({
-        content: 'There was an error while executing this command',
-        ephemeral: true});
+      // return if isSelectMenuEvent does not exist
+      if (!SelectMenuEvent) return;
+
+      // reply by executing
+      try {
+        SelectMenuEvent.execute(interaction);
+      } catch (error) {
+        console.error(error);
+        interaction.reply({
+          content: 'There was an error while executing this command',
+          ephemeral: true});
+      }
+    } else if (interaction.isButton()) {
+      const buttonEvent = interaction.client.buttons
+          .get(interaction.component.customId);
+
+      // return if isSelectMenuEvent does not exist
+      if (!buttonEvent) return;
+
+      // reply by executing
+      try {
+        buttonEvent.execute(interaction);
+      } catch (error) {
+        console.error(error);
+        interaction.reply({
+          content: 'There was an error while executing this command',
+          ephemeral: true});
+      }
     }
   },
 };
