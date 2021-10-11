0f5556ca77225a57505134de180d91077ce4dafe
LazyTurtleknight <tube3719@gmail.com>
2021-10-11 16:46:34 +0200

    Remove superfluous start subcommand

--- commands/werewolf.js
+++ commands/werewolf.js
@@ -2,7 +2,7 @@
 const {game, roles, subcommands, events} = require('../werewolf/constants.js');
 const {SlashCommandBuilder} = require('@discordjs/builders');
 const gameInstance = require('../werewolf/WerewolfGame.js');
-const {MessageActionRow, MessageSelectMenu, MessageButton} =
+const {MessageActionRow, MessageButton} =
     require('discord.js');
 
 module.exports = {
@@ -15,9 +15,6 @@ module.exports = {
       .addSubcommand((subcommand) => subcommand
           .setName(subcommands.create)
           .setDescription('Create a game.'))
-      .addSubcommand((subcommand) => subcommand
-          .setName(subcommands.start)
-          .setDescription('Starts the game.'))
       .addSubcommand((subcommand) => subcommand
           .setName(subcommands.end)
           .setDescription('Manually end the game.')),
@@ -56,56 +53,6 @@ module.exports = {
           console.error(error);
           interaction.reply(`Something went wrong while creating a game.`);
         }
-        break;
-
-      // start the game
-      case subcommands.start:
-        if (!gameInstance) {
-          interaction.reply(`Before starting a game` +
-          ` you need to create a game with the create command.`);
-        } else {
-          switch (gameInstance.status) {
-            case game.STATUS.inCreation:
-              // player need to select possible roles
-              const roleArray = [];
-              for (const key of Object.keys(roles)) {
-                roleArray.push({
-                  label: roles[key],
-                  description: roles[key],
-                  value: `Value of ${key}`,
-                });
-              }
-              const row = new MessageActionRow()
-                  .addComponents(
-                      new MessageSelectMenu()
-                          .setCustomId('select')
-                          .setPlaceholder('Possible roles')
-                          .setMinValues(0)
-                          .setMaxValues(gameInstance.getPlayerCount())
-                          .addOptions(roleArray),
-                  );
-              interaction.reply({
-                content: 'Choose possible roles.',
-                components: [row],
-                ephemeral: true,
-              });
-              gameInstance.start();
-              break;
-
-            case game.STATUS.running:
-              interaction.reply(`Game is already running.`);
-              break;
-
-            default:
-              interaction.reply(`Something unexpected happened.` +
-              `Try ending the game with the end command and ` +
-              `then create a new game with the create command.`);
-              console.log(gameInstance.status,
-                  interaction.options.getSubcommand());
-              break;
-          }
-        }
-
         break;
 
       // end the game
